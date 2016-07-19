8e88292a7daa5893eb1adda54f1acb06bf1e7bb9
Julien Bréhin <julbrehin@gmail.com>
2016-07-19 16:52:50 +0200

    Implemented unit tests for create_agent client handler
    + Implemented specified tests
    + Added test data embedded in the test file (maybe some of it should be moved out of the file, later on)
    + Created empty errors and client files from craftai package

--- tests/test_create_agent.py
+++ tests/test_create_agent.py
@@ -1,5 +1,75 @@
 import unittest
 
+from craftai import client as craft_client
+from craftai import errors as craft_err
+
+valid_id = "rainbow unicorn"
+valid_context = {
+    "presence": {
+        "type": "enum"
+    },
+    "lightIntensity": {
+        "type": "continuous"
+    },
+    "lightbulbColor": {
+        "type": "enum"
+    }
+}
+valid_output = ["lightbulbColor"]
+valid_tq = 100
+valid_model = {
+    "model": {
+        "context": valid_context,
+        "output": valid_output,
+        "time_quantum": valid_tq
+    }
+}
+
+invalid_ids = (("bad_id_type", 42),
+               ("empty_id", ""),
+               ("undefined_id", None))
+
+invalid_contexts = (("no_context", None),
+                    ("empty_context", {}),
+                    ("invalid_context_type", {
+                        "presence": {
+                            "type": "chair"
+                        },
+                        "lightIntensity": {
+                            "type": "continuous"
+                        },
+                        "lightbulbColor": {
+                            "type": "enum"
+                        }
+                    }),
+                    ("invalid_context_missing_type_key", {
+                        "context": {
+                            "presence": {
+                                "typo": "enum"
+                            },
+                            "lightIntensity": {
+                                "type": "continuous"
+                            },
+                            "lightbulbColor": {
+                                "type": "enum"
+                            }
+                        },
+                        "output": ["lightbulbColor"],
+                        "time_quantum": 100
+                    }))
+
+invalid_outputs = (("no_output", None),
+                   ("output_not_in_the_model", ["beerBrand"]))
+
+unspecified_models = (None,
+                      "",
+                      {})
+
+invalid_time_quanta = (("negative_tq", -42),
+                       ("null_tq", 0),
+                       ("high_tq", 4294967296),
+                       ("float_tq", 3.141592))
+
 
 class TestCreateAgent(unittest.TestSuite):
     """docstring for TestCreateAgent"""
@@ -17,7 +87,8 @@ class TestCreateAgentSuccess(unittest.TestCase):
         It should give a proper JSON response with `id` and
         `model` fields being strings.
         """
-        pass
+        resp = craft_client.create_agent(valid_model)
+        self.assertTrue(isinstance(resp.id, basestring))
 
     def test_create_agent_given_agent_id(self):
         """create_agent should succeed when given a string ID
@@ -26,7 +97,8 @@ class TestCreateAgentSuccess(unittest.TestCase):
         `model` fields being strings and `id` being the same as the one
         given as a parameter.
         """
-        pass
+        resp = craft_client.create_agent(valid_model, valid_id)
+        self.assertEqual(resp.id, valid_id)
 
 
 class TestCreateAgentFailure(unittest.TestCase):
@@ -39,24 +111,48 @@ class TestCreateAgentFailure(unittest.TestCase):
         an agent with an ID that is not of type string, since agent IDs
         should always be strings.
         """
-        pass
+        for inv_id in invalid_ids:
+            self.assertRaises(
+                craft_err.CraftAIBadRequestError,
+                craft_client.create_agent,
+                valid_model,
+                inv_id)
 
     def test_create_agent_with_existing_agent_id(self):
-        """create_agent should fail when given a ID that already exists
+        """create_agent should fail when given an ID that already exists
 
         It should raise an error upon request for creation of
         an agent with an ID that already exists, since agent IDs
         should always be unique.
         """
-        pass
+        # Calling create_agent a first time
+        craft_client.create_agent(valid_model, valid_id)
+        # Asserting that an error is risen the second time
+        self.assertRaises(
+            craft_err.CraftAIBadRequestError,
+            craft_client.create_agent,
+            valid_model,
+            valid_id)
 
     def test_create_agent_with_invalid_context(self):
-        """create_agent should fail when given an invalid context
+        """create_agent should fail when given an invalid or no context
 
         It should raise an error upon request for creation of
         an agent with no context or a context that is invalid.
         """
-        pass
+        for inv_context in invalid_contexts:
+            model = {
+                "model": {
+                    "context": inv_context,
+                    "output": ["lightbulbColor"],
+                    "time_quantum": 100
+                }
+            }
+            self.assertRaises(
+                craft_err.CraftAIBadRequestError,
+                craft_client.create_agent,
+                model,
+                valid_id)
 
     def test_create_agent_with_invalid_output(self):
         """create_agent should fail when given no or an invalid output
@@ -65,7 +161,19 @@ class TestCreateAgentFailure(unittest.TestCase):
         with no specified output or one that doesn't exist in the
         model, since it is a mandatory key in the model.
         """
-        pass
+        for inv_output in invalid_outputs:
+            model = {
+                "model": {
+                    "context": valid_context,
+                    "output": inv_output,
+                    "time_quantum": 100
+                }
+            }
+            self.assertRaises(
+                craft_err.CraftAIBadRequestError,
+                craft_client.create_agent,
+                model,
+                valid_id)
 
     def test_create_agent_with_undefined_model(self):
         """create_agent should fail when given no model key in the request body
@@ -74,7 +182,12 @@ class TestCreateAgentFailure(unittest.TestCase):
         no model key in the request body, since it is a mandatory field to
         create an agent.
         """
-        pass
+        for empty_model in unspecified_models:
+            self.assertRaises(
+                craft_err.CraftAIBadRequestError,
+                craft_client.create_agent,
+                empty_model,
+                valid_id)
 
     def test_create_agent_with_invalid_time_quantum(self):
         """create_agent should fail when given an invalid time quantum
@@ -83,4 +196,16 @@ class TestCreateAgentFailure(unittest.TestCase):
         an incorrect time quantum in the model, since it is essential to
         perform any action with craft ai.
         """
-        pass
+        for inv_tq in invalid_time_quanta:
+            model = {
+                "model": {
+                    "context": valid_context,
+                    "output": valid_output,
+                    "time_quantum": inv_tq
+                }
+            }
+            self.assertRaises(
+                craft_err.CraftAIBadRequestError,
+                craft_client.create_agent,
+                model,
+                valid_id)
