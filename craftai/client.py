0e004a069860b4c6a614bf5dbaecd960132bb0ee
Julien Bréhin <julbrehin@gmail.com>
2016-07-27 11:33:52 +0200

    Started client implementation with proper config setup
    * CraftAIClient instantation calls set_config and checks that the config object given as parameters is fine to work with
    * Instatiation of a CraftAIClient and set_config should raise an error if the config object doesn't match the requirements

--- craftai/client.py
+++ craftai/client.py
@@ -1,12 +1,32 @@
+import six
+
+from craftai.errors import CraftAICredentialsError
+from craftai.errors import CraftAIBadRequestError
 
 
 class CraftAIClient():
     """docstring for CraftAIClient"""
     def __init__(self, cfg):
         super(CraftAIClient, self).__init__()
-        self.config = cfg
+        try:
+            self.set_config(cfg)
+        except (CraftAICredentialsError, CraftAIBadRequestError) as e:
+            raise e
 
-    def config(self, cfg):
+    def set_config(self, cfg):
+        if (not isinstance(cfg.get("token"), six.string_types)):
+            raise CraftAICredentialsError("""Unable to create client with no"""
+                                          """ or invalid token provided.""")
+        if (not isinstance(cfg.get("owner"), six.string_types)):
+            raise CraftAICredentialsError("""Unable to create client with no"""
+                                          """ or invalid owner provided.""")
+        if (not isinstance(cfg.get("url"), six.string_types)):
+            raise CraftAIBadRequestError("""Unable to create client with no"""
+                                         """ or invalid url provided.""")
+        if (cfg.get("url")[-1] != '/'):
+            raise CraftAIBadRequestError("""Unable to create client with"""
+                                         """ invalid url provided. The url"""
+                                         """ should terminate with a slash.""")
         self.cfg = cfg
 
     def create_agent(self, model, agent_id=""):
