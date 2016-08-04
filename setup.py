e2699966f6a2cd956ec886ce1b8e32e3536fa597
Julien Bréhin <julbrehin@gmail.com>
2016-08-04 15:36:10 +0200

    Changed call to update_readme
    * Now it should be called though a main craft-ai command with option --update_readme

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'update_readme= cli:update_readme'
+            'craft-ai= craftai.cli:main'
         ]
     }
 )
