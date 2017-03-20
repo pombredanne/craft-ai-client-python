a480c12c6dc781e33e1de384b5728f6a1f8f26d1
Sylvain Marchienne <sylvain.marchienne@gmail.com>
2017-03-20 11:57:03 +0100

    reimplementation of Interpreter._rebuild_context

--- tests/test_decide.py
+++ tests/test_decide.py
@@ -70,9 +70,12 @@ class TestDecide(unittest.TestCase):
                 "speed": {
                     "type": "continuous"
                 },
+                "day_of_week": {
+                    "type": "day_of_week",
+                    "is_generated": False
+                },
                 "month_of_year": {
-                    "type": "month_of_year",
-                    "is_generated": True
+                    "type": "month_of_year"
                 },
                 "timezone": {
                     "type": "timezone"
@@ -81,16 +84,44 @@ class TestDecide(unittest.TestCase):
             "output": ["speed"],
             "time_quantum": 500
         }
-        state = { "car": "Renault" }
+
+        # Case 1:
+        # - we do not provide a Time object while properties in configuration need to be generated from it
+        # - we do not provide those properties directly in the context
+        state = { "car": "Renault", "day_of_week": 2 }
+        self.assertRaises(
+            craft_err.CraftAIDecisionError,
+            Interpreter._rebuild_context,
+            configuration,
+            state)
+        print("Successfully raises CraftAIDecisionError.")
+
+        # Case 2:
+        # - we do provide a Time object while it's needed
+        # - in the context, we set a value that should be generated if not provided,
+        #   but we provide it and we don't want it to be replaced by generated value
+        state = { "car": "Renault", "day_of_week": 2, "timezone": "+02:00" }
         time = Time(1489998174, "+01:00")
-
-        rebuilt_context = Interpreter._rebuild_context(configuration, (state, time))
-
         expected_context = {
             "car": "Renault",
+            "day_of_week": 2,
+            "month_of_year": 3,
+            "timezone": "+02:00" # must not be replaced by +01:00
+        }
+        rebuilt_context = Interpreter._rebuild_context(configuration, state, time)
+        for x in expected_context.keys():
+            self.assertEqual(rebuilt_context[x], expected_context[x])
+
+        # Case 3:
+        # - we provide none of the properties that should be generated
+        state = { "car": "Renault", "day_of_week": 2 }
+        time = Time(1489998174, "+01:00")
+        rebuilt_context = Interpreter._rebuild_context(configuration, state, time)
+        expected_context = {
+            "car": "Renault",
+            "day_of_week": 2,
             "month_of_year": 3,
             "timezone": "+01:00"
         }
-
         for x in expected_context.keys():
             self.assertEqual(rebuilt_context[x], expected_context[x])
