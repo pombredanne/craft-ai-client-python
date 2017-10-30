efc6e13ee65269af5e87447e66eb9dfd551a72ca
Clodéric Mars <cloderic.mars@craft.ai>
2017-10-30 13:45:54 +0100

    Refactor `get_decision_tree` tests to use simpler functional interface

--- tests/test_get_decision_tree.py
+++ tests/test_get_decision_tree.py
@@ -1,93 +1,66 @@
-import unittest
-
 import craftai
+
+from nose.tools import assert_is_instance, assert_not_equal, assert_raises, with_setup
 
 from . import settings
 from .data import valid_data
 from .data import invalid_data
 
-class TestGetDecisionTreeSuccess(unittest.TestCase):
-  """Checks that the client succeeds when retrieving an agent's decision tree
-  at a given timestamp, with OK input.
+CLIENT = craftai.Client(settings.CRAFT_CFG)
+AGENT_ID = "test_get_decision_tree_" + settings.RUN_ID
+
+def setup_agent_w_operations():
+  CLIENT.delete_agent(AGENT_ID)
+  CLIENT.create_agent(valid_data.VALID_CONFIGURATION, AGENT_ID)
+  CLIENT.add_operations(AGENT_ID, valid_data.VALID_OPERATIONS_SET)
+
+def teardown():
+  CLIENT.delete_agent(AGENT_ID)
+
+@with_setup(setup_agent_w_operations, teardown)
+def test_get_decision_tree_with_correct_input():
+  decision_tree = CLIENT.get_decision_tree(
+    AGENT_ID,
+    valid_data.VALID_TIMESTAMP)
+
+  assert_is_instance(decision_tree, dict)
+  assert_not_equal(decision_tree.get("_version"), None)
+  assert_not_equal(decision_tree.get("configuration"), None)
+  assert_not_equal(decision_tree.get("trees"), None)
+
+@with_setup(setup_agent_w_operations, teardown)
+def test_get_decision_tree_with_invalid_id():
+  """get_decision_tree should fail when given a non-string/empty string ID
+
+  It should raise an error upon request for retrieval of an agent's
+  decision tree with an ID that is not of type string, since agent IDs
+  should always be strings.
   """
-  @classmethod
-  def setUpClass(cls):
-    cls.client = craftai.Client(settings.CRAFT_CFG)
-    cls.agent_id = valid_data.VALID_ID  + "_" + settings.RUN_ID
-
-  def setUp(self):
-    self.client.delete_agent(self.agent_id)
-    self.client.create_agent(valid_data.VALID_CONFIGURATION, self.agent_id)
-    self.client.add_operations(
-      self.agent_id,
-      valid_data.VALID_OPERATIONS_SET)
-
-  def tearDown(self):
-    self.client.delete_agent(self.agent_id)
-
-  def test_get_decision_tree_with_correct_input(self):
-    """get_decision_tree should succeed when given proper ID and timestamp.
-
-    It should give a proper JSON object response with 3 elements: _version, configuration and trees.
-    """
-    decision_tree = self.client.get_decision_tree(
-      self.agent_id,
+  for empty_id in invalid_data.UNDEFINED_KEY:
+    assert_raises(
+      craftai.errors.CraftAiBadRequestError,
+      CLIENT.get_decision_tree,
+      invalid_data.UNDEFINED_KEY[empty_id],
       valid_data.VALID_TIMESTAMP)
 
-    self.assertIsInstance(decision_tree, dict)
-    self.assertNotEqual(decision_tree.get("_version"), None)
-    self.assertNotEqual(decision_tree.get("configuration"), None)
-    self.assertNotEqual(decision_tree.get("trees"), None)
+@with_setup(setup_agent_w_operations, teardown)
+def test_get_decision_tree_with_unknown_id():
+  """get_decision_tree should fail when given an unknown agent ID
 
+  It should raise an error upon request for the retrieval of an agent
+  that doesn't exist.
+  """
+  assert_raises(
+    craftai.errors.CraftAiNotFoundError,
+    CLIENT.get_decision_tree,
+    invalid_data.UNKNOWN_ID,
+    valid_data.VALID_TIMESTAMP)
 
-class TestGetDecisionTreeFailure(unittest.TestCase):
-  """Checks that the client fails properly when getting an agent's decision
-  tree at a given timestamp with bad input."""
-  @classmethod
-  def setUpClass(cls):
-    cls.client = craftai.Client(settings.CRAFT_CFG)
-    cls.agent_id = valid_data.VALID_ID  + "_" + settings.RUN_ID
-
-  def setUp(self):
-    self.client.delete_agent(self.agent_id)
-    self.client.create_agent(valid_data.VALID_CONFIGURATION, self.agent_id)
-    self.client.add_operations(
-      self.agent_id,
-      valid_data.VALID_OPERATIONS_SET)
-
-  def tearDown(self):
-    self.client.delete_agent(self.agent_id)
-
-  def test_get_decision_tree_with_invalid_id(self):
-    """get_decision_tree should fail when given a non-string/empty string ID
-
-    It should raise an error upon request for retrieval of an agent's
-    decision tree with an ID that is not of type string, since agent IDs
-    should always be strings.
-    """
-    for empty_id in invalid_data.UNDEFINED_KEY:
-      self.assertRaises(
-        craftai.errors.CraftAiBadRequestError,
-        self.client.get_decision_tree,
-        invalid_data.UNDEFINED_KEY[empty_id],
-        valid_data.VALID_TIMESTAMP)
-
-  def test_get_decision_tree_with_unknown_id(self):
-    """get_decision_tree should fail when given an unknown agent ID
-
-    It should raise an error upon request for the retrieval of an agent
-    that doesn't exist.
-    """
-    self.assertRaises(
-      craftai.errors.CraftAiNotFoundError,
-      self.client.get_decision_tree,
-      invalid_data.UNKNOWN_ID,
-      valid_data.VALID_TIMESTAMP)
-
-  def test_get_decision_tree_with_invalid_timestamp(self):
-    for inv_ts in invalid_data.INVALID_TIMESTAMPS:
-      self.assertRaises(
-        craftai.errors.CraftAiBadRequestError,
-        self.client.get_decision_tree,
-        self.agent_id,
-        invalid_data.INVALID_TIMESTAMPS[inv_ts])
+@with_setup(setup_agent_w_operations, teardown)
+def test_get_decision_tree_with_invalid_timestamp():
+  for inv_ts in invalid_data.INVALID_TIMESTAMPS:
+    assert_raises(
+      craftai.errors.CraftAiBadRequestError,
+      CLIENT.get_decision_tree,
+      AGENT_ID,
+      invalid_data.INVALID_TIMESTAMPS[inv_ts])
