391a2fcdcb61e3aa8ec906c784d9e6deb1af96ca
tom <tom.puech@gmail.com>
2018-04-11 11:32:27 +0200

    add six.text_type in order to handle unicode type

--- craftai/pandas/utils.py
+++ craftai/pandas/utils.py
@@ -1,11 +1,20 @@
 import pandas as pd
+import six
 
 DUMMY_COLUMN_NAME = "CraftGeneratedDummy"
 
 def is_valid_property_value(key, value):
   # From https://stackoverflow.com/a/19773559
+  # https://pythonhosted.org/six/#six.text_type for unicode in Python 2
   return key != DUMMY_COLUMN_NAME and \
-         ((not hasattr(value, "__len__") or isinstance(value, str)) and pd.notnull(value))
+         ( \
+           ( \
+            not hasattr(value, "__len__") \
+            or isinstance(value, str) \
+            or isinstance(value, six.text_type) \
+           ) \
+           and pd.notnull(value) \
+         )
 
 # Helper
 def create_timezone_df(df, name):
