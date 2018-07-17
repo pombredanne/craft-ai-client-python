cc0545926ee8202c1ba5fad31513963b263f0379
Guillaume <hattenberger@telecom-paristech.fr>
2018-07-17 15:22:35 +0200

    Add time creation tests with 2 strings

--- tests/test_time.py
+++ tests/test_time.py
@@ -25,6 +25,9 @@ class TestTime(unittest.TestCase):
     self.assertEqual(Time(1356998400, timezone="CST").timezone, "-06:00")
     self.assertEqual(Time("1977-04-22T01:00:00-0300").timezone, "-03:00")
     self.assertEqual(Time("1977-04-22T01:00:00+1000").timezone, "+10:00")
+    self.assertEqual(Time("2011-04-22 01:00:00+0900").timezone, "+09:00")
+    self.assertEqual(Time(t="2017-01-01 00:00:00", timezone="-03:00").timezone, "-03:00")
+    self.assertEqual(Time(t="2017-01-01 03:00:00", timezone="+02:00").timezone, "+02:00")
     self.assertEqual(Time().timezone, Time().timezone)
 
   def test_parser_format(self):
@@ -32,3 +35,5 @@ class TestTime(unittest.TestCase):
     self.assertRaises(CraftAiTimeError, Time, "2010-01-01T07:30:30")
     # Not ISO format
     self.assertRaises(CraftAiTimeError, Time, "22-04-1977T01:00:00+0100")
+    # Two timezones
+    self.assertRaises(CraftAiTimeError, Time, "2011-04-22 01:00:00+0900", "-03:00")
