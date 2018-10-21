f7a4a3666afff8768130aff2ccdb8a8bb6a9e169
Anthony D'Amato <>
2018-10-21 23:16:43 +0200

    add tree display to the pandas client

--- craftai/pandas/utils.py
+++ craftai/pandas/utils.py
@@ -1,5 +1,7 @@
+import json
 import pandas as pd
 import six
+from IPython.core.display import display, HTML
 
 DUMMY_COLUMN_NAME = "CraftGeneratedDummy"
 
@@ -21,3 +23,46 @@ def create_timezone_df(df, name):
   else:
     timezone_df[name] = df.index.strftime("%z")
   return timezone_df
+
+# Display the given tree
+
+def display_tree(decision_tree, configuration=None):
+  html_template = """ <html>
+  <body>
+    <div id="tree-div">
+    </div>
+    <script src="https://unpkg.com/react@15/dist/react.min.js">
+    </script>
+    <script src="https://unpkg.com/react-dom@15/dist/react-dom.min.js">
+    </script>
+    <script src="https://unpkg.com/glamor@2/umd/index.min.js">
+    </script>
+    <script src="https://unpkg.com/glamorous@4/dist/glamorous.umd.min.js">
+    </script>
+    <script src="https://d3js.org/d3.v4.min.js">
+    </script>
+    <script src="https://unpkg.com/react-craft-ai-decision-tree">
+    </script>
+    <script>
+    var tree = "json_arbre_ici"
+  ReactDOM.render(
+          React.createElement(DecisionTree, {element}),
+          document.getElementById('tree-div')
+        );
+    </script>
+  </body>
+  </html>"""
+
+  if configuration:
+    decision_tree["configuration"] = configuration
+  else:
+    configuration = decision_tree["configuration"]
+
+  # If it is a Standalone Agent tree, change 'Standalone agent' to the
+  # actual output name - Suppose that there is a unique output here.
+  if "Standalone_agent" in decision_tree["trees"].keys():
+    decision_tree["trees"][configuration["output"][0]] = decision_tree["trees"].pop("Standalone_agent")
+
+  html_tree = html_template.format(element="{height: 500, data: "+json.dumps(decision_tree)+"}")
+  return html_tree
+  display(HTML(html_tree))
