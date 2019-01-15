67ec763f62f689605738301ee2cbd4330fd3ad56
Anthony <anthony.damato@hotmail.fr>
2019-01-15 19:08:39 +0100

    delete is_null operator because 'is' operator will be used for null values

--- craftai/operators.py
+++ craftai/operators.py
@@ -3,7 +3,6 @@ OPERATORS = {
   "IN": "[in[",
   "GTE": ">=",
   "LT": "<",
-  "ISNULL": "is_null"
 }
 
 OPERATORS_FUNCTION = {
@@ -12,6 +11,5 @@ OPERATORS_FUNCTION = {
   OPERATORS["LT"]: lambda context, value: context < value,
   OPERATORS["IN"]: lambda context, value:
                    context >= value[0] and context < value[1] if value[0] < value[1]
-                   else context >= value[0] or context < value[1],
-  OPERATORS["ISNULL"]: lambda context, value: context is None and value is None,
+                   else context >= value[0] or context < value[1]
 }
